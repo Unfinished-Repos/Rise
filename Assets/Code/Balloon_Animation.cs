39849b3abc4cd925850f36bacd44a76e6d044d0f
Andrew Friedman <andrewfriedman01@gmail.com>
2023-01-02 23:50:51 -0500

    Add content

---
+++ Assets/Code/Balloon_Animation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Balloon_Animation : MonoBehaviour {
+
+	SpriteRenderer Balloon;
+	public Sprite Frame_0;
+	public Sprite Frame_1;
+	public Sprite Frame_2;
+	public Sprite Frame_3;
+	public Sprite Frame_4;
+	public Sprite Frame_5;
+	public Sprite Frame_6;
+	public Sprite Frame_7;
+	float Delay = .15f;
+
+	void Start () {
+		Balloon = this.gameObject.GetComponent<SpriteRenderer>();
+		StartCoroutine(Animate());
+	}
+
+	IEnumerator Animate () {
+		while (1 > 0) {
+			Balloon.sprite = Frame_0;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_1;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_2;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_3;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_4;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_5;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_6;
+			yield return new WaitForSecondsRealtime(Delay);
+			Balloon.sprite = Frame_7;
+			yield return new WaitForSecondsRealtime(Delay);
+		}
+	}
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controls : MonoBehaviour {
+	float[] Mouse_Position_Old;
+	float[] Mouse_Position_New;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mouse_Position_New[1] = Input.mousePosition.x;
+		Mouse_Position_New[2] = Input.mousePosition.y;
+		if (Input.GetMouseButton(0)) {
+
+		}
+		Mouse_Position_Old[1] = Input.mousePosition.x;
+		Mouse_Position_Old[2] = Input.mousePosition.y;
+	}
+}
